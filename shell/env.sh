f644a6f9d578800950f85a8c1e67d2e93e1ce6bb
fivvwsl <root@fiv>
2025-09-03 17:16:52 +0800

    update env.sh

--- shell/env.sh
+++ shell/env.sh
@@ -1,10 +1,11 @@
+#!/usr/bin/env bash
 set -ex
 
-if command --v ruby &> /dev/null
+if command -v ruby >/dev/null 2>&1; then
   echo "-->> Ruby is installed"
 else
   echo "-->> Ruby is not installed"
-  sudo apt install ruby-full
+  sudo apt install -y ruby-full
 fi
 
 gem install jekyll
@@ -12,7 +13,7 @@ gem install bundler
 gem install github-pages
 
 # if jekyll -v; then
-if command -v jekyll &> /dev/null
+if command -v jekyll >/dev/null 2>&1; then
   echo "-->> Jekyll is installed"
 else
   echo "-->> Jekyll is not installed"
